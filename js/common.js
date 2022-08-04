ea6898cae3d51cf261e73f8dff2b81d440a9082c
Rosiealan <516308181@qq.com>
2022-08-04 20:11:30 +0800

    完成登录界面

--- js/common.js
+++ js/common.js
@@ -7,3 +7,10 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 // 每个页面都会导入该js 统一的设置写这里即可
+
+const myToast=new bootstrap.Toast(document.querySelector('#myToast'),{delay:2000,})
+      function tip(msg){
+        document.querySelector('#myToast .toast-body').innerHTML=msg
+        myToast.show()
+      }
+      tip('保持核酸24分钟')
