4501dfc65008ed2e50793db3edce936e29743230
Rosiealan <516308181@qq.com>
2022-08-05 17:19:24 +0800

    最后一次

--- js/common.js
+++ js/common.js
@@ -31,3 +31,15 @@ logoutBtn&&(logoutBtn.onclick=function(){
   localStorage.removeItem('token')
   window.location.assign('./login.html')
 })
+
+axios.interceptors.response.use(function (response) {
+  // 2xx 范围内的状态码都会触发该函数。
+  // 对响应数据做点什么
+  console.log('响应拦截器-第一个回调函数')
+  console.log('response:', response)
+  return response.data;
+}, function (error) {
+  // 超出 2xx 范围的状态码都会触发该函数。
+  // 对响应错误做点什么
+  return Promise.reject(error.response);
+});
